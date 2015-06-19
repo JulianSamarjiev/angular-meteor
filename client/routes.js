107725936c7c942dc92bee42bd68abd14a55d4ca
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-06-19 20:10:34 +0200

    Authentication with routers

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,13 @@
+angular.module("socially").run(["$rootScope", "$state", function($rootScope, $state) {
+  $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, erroe) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === "AUTH_REQUIRED") {
+      $state.go('parties');
+    }
+  });
+}]);
+
 angular.module("socially").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
 function($urlRouterProvider, $stateProvider, $locationProvider){
 
@@ -12,7 +22,12 @@ function($urlRouterProvider, $stateProvider, $locationProvider){
     .state('partyDetails', {
       url: '/parties/:partyId',
       templateUrl: 'client/parties/views/party-details.ng.html',
-      controller: 'PartyDetailsCtrl'
+      controller: 'PartyDetailsCtrl',
+      resolve: {
+        "currentUser": ["$meteor", function($meteor){
+          return $meteor.requireUser();
+        }]
+      }
     });
 
     $urlRouterProvider.otherwise("/parties");
