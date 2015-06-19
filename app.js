f142b81716e5fcd17fe1d5ad6ea62931662069a3
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-06-19 19:24:05 +0200

    routing and multiple views

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ if (Meteor.isClient) {
     }
 
   }]);
+
+  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', function($scope, $stateParams){
+
+    $scope.partId = $stateParams.partyId;
+
+  }]);
 }
 
 if (Meteor.isServer) {
