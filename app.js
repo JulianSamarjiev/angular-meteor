fa428d18f2c70d0d77a31d9ec7522ae5d12e9d73
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-06-19 19:36:10 +0200

    binding objects

--- app.js
+++ app.js
@@ -38,11 +38,25 @@ if (Meteor.isClient) {
 
   }]);
 
-  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', function($scope, $stateParams){
+  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor', function($scope, $stateParams, $meteor){
 
-    $scope.partId = $stateParams.partyId;
+    $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+
+    $scope.save = function() {
+      $scope.party.save().then(function(numberOfDocks) {
+        console.log('save success doc affected', numberOfDocs);
+      }, function(error){
+        console.log('save error', error);
+      });
+    };
+
+    $scope.reset = function() {
+      $scope.party.reset();
+    };
 
   }]);
+
+
 }
 
 if (Meteor.isServer) {
